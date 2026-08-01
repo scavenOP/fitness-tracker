4ad0493655774f0c56a2fc156aa89f5bd24ea9ac
Niladri Samanta <niladrisamanta4@gmail.com>
2026-08-01 15:49:17 +0530

    update

--- scripts/patch_gradle.py
+++ scripts/patch_gradle.py
@@ -1,23 +1,39 @@
-import os
-
 path = 'mobile-app/android/app/build.gradle'
-gradle = open(path).read()
+content = open(path).read()
 
-if 'signingConfigs' not in gradle:
-    home = 'System.getProperty("user.home")'
-    signing_block = (
-        '\nsigningConfigs {\n'
-        '    debug {\n'
-        '        storeFile file(' + home + ' + "/.android/debug.keystore")\n'
-        '        storePassword "android"\n'
-        '        keyAlias "androiddebugkey"\n'
-        '        keyPassword "android"\n'
-        '    }\n'
-        '}\n'
-    )
-    gradle = gradle.replace('android {', 'android {' + signing_block, 1)
-    gradle = gradle.replace('debug {', 'debug {\n            signingConfig signingConfigs.debug', 1)
-    open(path, 'w').write(gradle)
-    print('Gradle patched')
-else:
+if 'signingConfigs' in content:
     print('Gradle already patched')
+    exit(0)
+
+home = 'System.getProperty("user.home")'
+signing_configs = (
+    '    signingConfigs {\n'
+    '        debug {\n'
+    '            storeFile file(' + home + ' + "/.android/debug.keystore")\n'
+    '            storePassword "android"\n'
+    '            keyAlias "androiddebugkey"\n'
+    '            keyPassword "android"\n'
+    '        }\n'
+    '    }\n'
+)
+
+# Insert signingConfigs block before buildTypes
+if '    buildTypes {' in content:
+    content = content.replace('    buildTypes {', signing_configs + '    buildTypes {', 1)
+else:
+    # fallback: different indentation
+    content = content.replace('buildTypes {', signing_configs + 'buildTypes {', 1)
+
+# Insert signingConfig signingConfigs.debug as first line inside buildTypes > debug
+# Find "debug {" that comes AFTER "buildTypes {"
+bt_index = content.find('buildTypes {')
+debug_index = content.find('debug {', bt_index)
+if debug_index != -1:
+    insert_at = content.index('\n', debug_index) + 1
+    content = content[:insert_at] + '            signingConfig signingConfigs.debug\n' + content[insert_at:]
+
+open(path, 'w').write(content)
+print('Gradle patched successfully')
+print('--- Patched buildTypes section ---')
+bt = content[content.find('buildTypes {'):content.find('buildTypes {')+300]
+print(bt)
